d60cb1c420ffe94bf30f6aef2ecbe13721c0e4a0
unknown <quansahd577@gmail.com>
2024-02-28 10:47:52 +0000

    Portfolio added

--- app.js
+++
@@ -1,8 +0,0 @@
-const Btn=document.querySelector('.mobile');
-const Menu=document.querySelector('.menu');
-Btn.addEventListener('click',()=>{
-    const Toggled=Menu.classList.toggle('menu-toggle');
-    const Text=Toggled ? Btn.innerHTML='&times;':Btn.innerHTML=
-    '&equiv;';
-});
-
